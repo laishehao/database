1cf96772237efc757a6878615d26681077e7a0a4
Bricks0411 <1072391442@qq.com>
2026-01-04 21:01:10 +0800

    Update 新增触发器：处理write表发生改变后，对应作业的提交数

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -40,7 +40,7 @@ BEGIN
         INSERT INTO Error_Log (error_time, message) VALUES (NOW(), '触发器执行失败');
     END;
     
-    -- 插入该课程已发布的所有作业的write记录
+    -- 插入该课程已发布的所有作业的write记录，如果出错，也不会插入无效数据
     INSERT INTO `Write` (Wno, Sno, State, Wrcontent, Score)
     SELECT 
         W.Wno,
@@ -51,3 +51,23 @@ BEGIN
     FROM Work W
     WHERE W.Cno = NEW.Cno AND W.Wprogress > 0;  -- 只为已发布的作业创建记录
 END$$
+
+-- 触发器：处理作业完成数量，例如write中产生变化，则更新Work表中的Wprogress字段
+DELIMITER $$
+CREATER TRIGGER trg_after_write_update
+AFTER UPDATE ON `Write`
+FOR EACH ROW
+BEGIN
+    DECLARE total_count INT;
+    DECLARE completed_count INT;
+
+    -- 计算该作业的总学生数和已完成学生数
+    SELECT COUNT(*) INTO total_count FROM `Write` WHERE Wno = NEW.Wno;
+    SELECT COUNT(*) INTO completed_count FROM `Write` WHERE Wno = NEW.Wno AND State = 1;
+
+    -- 更新work表中的Wprogress字段为已完成学生数
+    UPDATE Work
+    SET Wprogress = completed_count
+    WHERE Wno = NEW.Wno;
+END$$
+DELIMITER ;
